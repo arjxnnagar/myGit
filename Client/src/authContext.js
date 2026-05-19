383dea79914f99be4e56ec81f29e8607adcf71b9
Arjun Nagar <arjunnagar012@gmail.com>
2026-05-19 19:57:55 +0530

    Frontend Started

---
+++ Client/src/authContext.js
@@ -0,0 +1,25 @@
+import React, { Children } from "react";
+import { createContext,useState,useEffect,useContext } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = ()=>{
+    return useContext(AuthContext);
+}
+
+export const AuthProvider = ({Children})=>{
+
+    const [currentUser,setCurrentUser] = useState(null);
+    useEffect(()=>{
+        const userId = localStorage.getItem('userId');
+        if(userId){
+            setCurrentUser(userId);
+        }
+    },[]);
+
+    const value = {
+        currentUser , setCurrentUser
+    }
+
+    return <AuthContext.Provider value = {value}>{Children}</AuthContext.Provider>
+}
